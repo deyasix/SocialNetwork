3164ecd368df07c76646a5f1e801f47f1486b4b1
deyasix <akikidzhan@gmail.com>
2023-05-05 23:55:20 +0300

    refactoring, change SharedPreferences to DataStore

--- app/src/main/java/com/example/myprofilemarkup/AuthActivity.kt
+++ app/src/main/java/com/example/myprofilemarkup/AuthActivity.kt
@@ -1,53 +1,82 @@
 package com.example.myprofilemarkup
 
-import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
+import android.util.Log
 import android.util.Patterns
 import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.stringPreferencesKey
+import androidx.datastore.preferences.preferencesDataStore
+import androidx.lifecycle.lifecycleScope
 import com.example.myprofilemarkup.databinding.ActivityRegistrationBinding
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.launch
 
 class AuthActivity : AppCompatActivity() {
 
+    companion object {
+        const val PREFERENCE_NAME = "MyProfileMarkupPreference"
+    }
+
     private lateinit var binding: ActivityRegistrationBinding
+
+    private val dataStore by preferencesDataStore(PREFERENCE_NAME)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val sharedPreference =  getSharedPreferences(getString(R.string.preference_file_key),
-            Context.MODE_PRIVATE)
-        val email = sharedPreference.getString("E-mail", null)
+        var email: String? = null
+        lifecycleScope.launch {
+            try {
+                email = dataStore.data.first()[stringPreferencesKey("email")]
+            } catch (ex: Exception) {
+                Log.e(ex.message, ex.toString())
+            }
+        }
         if (email != null) {
-            val intent = Intent(this, MainActivity::class.java)
-            intent.putExtra("nameSurname", getNameSurname(email))
-            startActivity(intent)
-        } else {
-            binding = ActivityRegistrationBinding.inflate(layoutInflater)
-            setContentView(binding.root)
-            setupListeners()
-            binding.registrationButton.setOnClickListener(this::registrationButtonClickEvent)
+            navigateToMain(email.toString())
+        }
+        binding = ActivityRegistrationBinding.inflate(layoutInflater)
+        restoreState(savedInstanceState)
+        setContentView(binding.root)
+        setupListeners()
+    }
+
+    private fun restoreState(savedInstanceState: Bundle?) {
+        if (savedInstanceState != null) {
+            binding.textInputEditTextEmail.setText(savedInstanceState.getString("EMAIL"))
+            if (binding.textInputEditTextEmail.text?.isNotEmpty() == true) validateEmail()
+            binding.textInputEditTextPassword.setText(savedInstanceState.getString("PASSWORD"))
+            if (binding.textInputEditTextPassword.text?.isNotEmpty() == true) validatePassword()
+            binding.checkBoxRemember.isChecked = savedInstanceState.getBoolean("REMEMBER")
         }
     }
 
-    private fun registrationButtonClickEvent(view: View) {
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString("EMAIL", binding.textInputEditTextEmail.text.toString())
+        outState.putString("PASSWORD", binding.textInputEditTextPassword.text.toString())
+        outState.putBoolean("REMEMBER", binding.checkBoxRemember.isChecked)
+    }
+
+    private fun registrationButtonClickEvent() {
         if (isValidate()) {
-            val sharedPreference =  getSharedPreferences(getString(R.string.preference_file_key),
-                Context.MODE_PRIVATE)
+            val email = binding.textInputEditTextEmail.text.toString()
             if (binding.checkBoxRemember.isChecked) {
-                with (sharedPreference.edit()) {
-                    putString("E-mail", binding.textInputEditTextEmail.text.toString())
-                    apply()
+                lifecycleScope.launch {
+                    dataStore.edit { settings ->
+                        settings[stringPreferencesKey("email")] = email}
                 }
             }
-            val intent = Intent(this, MainActivity::class.java)
-            intent.putExtra("nameSurname", getNameSurname(binding.textInputEditTextEmail.text.toString()))
-            startActivity(intent)
-            overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_right);
+            navigateToMain(email)
+            overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_right)
         } else {
-            Toast.makeText(this, "Incorrect e-mail or password!", Toast.LENGTH_LONG).show()
+            Toast.makeText(this, resources.getString(R.string.incorrect_password_email), Toast.LENGTH_LONG).show()
         }
     }
 
@@ -55,83 +84,78 @@ class AuthActivity : AppCompatActivity() {
         val splitEmail = email.split("@")[0]
         return if (splitEmail.contains(".")) {
             val nameSurname = splitEmail.split(".")
-            val name = nameSurname[0]
-            val surname = nameSurname[1]
+            val (name, surname) = Pair(nameSurname[0], nameSurname[1])
             name.replaceFirstChar { it.uppercase() } + " " + surname.replaceFirstChar { it.uppercase() }
         } else splitEmail.replaceFirstChar { it.uppercase() }
     }
 
-    private fun isValidate(): Boolean = validateEmail() && validatePassword()
+    private fun isValidate(): Boolean {
+        val isValidEmail = validateEmail()
+        val isValidPassword = validatePassword()
+        return isValidEmail && isValidPassword
+    }
 
     private fun setupListeners() {
         binding.textInputEditTextEmail.addTextChangedListener(TextFieldValidation(binding.textInputEditTextEmail))
         binding.textInputEditTextPassword.addTextChangedListener(TextFieldValidation(binding.textInputEditTextPassword))
+        binding.registrationButton.setOnClickListener { registrationButtonClickEvent() }
+    }
+
+    private fun navigateToMain(email: String) {
+        val intent = Intent(this, MainActivity::class.java)
+        intent.putExtra("nameSurname", getNameSurname(email))
+        startActivity(intent)
     }
 
     inner class TextFieldValidation(private val view: View) : TextWatcher {
-        override fun afterTextChanged(s: Editable?) {}
-        override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+        override fun afterTextChanged(s: Editable?) {/* no action */}
+        override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {/* no action */}
         override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-            when (view.id) {
-                R.id.textInputEditTextEmail -> {
+            when (view) {
+                binding.textInputEditTextEmail -> {
                     validateEmail()
                 }
 
-                R.id.textInputEditTextPassword -> {
+                binding.textInputEditTextPassword -> {
                     validatePassword()
                 }
             }
+
         }
     }
 
     private fun validateEmail(): Boolean {
-        if (binding.textInputEditTextEmail.text.toString().trim().isEmpty()) {
-            binding.textInputLayoutEmail.error = "Required Field!"
-            binding.textInputEditTextEmail.requestFocus()
-            return false
-        } else if (!isValidEmail(binding.textInputEditTextEmail.text.toString())) {
-            binding.textInputLayoutEmail.error = "Invalid Email!"
-            binding.textInputEditTextEmail.requestFocus()
-            return false
-        } else {
+        val email = binding.textInputEditTextEmail.text.toString()
+        val isEmptyEmail = email.trim().isEmpty()
+        return if (!isEmptyEmail && Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
             binding.textInputLayoutEmail.isErrorEnabled = false
+            true
+        } else {
+            binding.textInputLayoutEmail.error = when {
+                isEmptyEmail -> resources.getString(R.string.required_field)
+                else -> resources.getString(R.string.invalid_email)
+            }
+            if (!isEmptyEmail) binding.textInputEditTextEmail.requestFocus()
+            false
         }
-        return true
     }
 
     private fun validatePassword(): Boolean {
-        if (binding.textInputEditTextPassword.text.toString().trim().isEmpty()) {
-            binding.textInputLayoutPassword.error = "Required Field!"
-            binding.textInputEditTextPassword.requestFocus()
-            return false
-        } else if (binding.textInputEditTextPassword.text.toString().length < 8) {
-            binding.textInputLayoutPassword.error = "password can't be less than 8"
-            binding.textInputEditTextPassword.requestFocus()
-            return false
-        } else if (!isStringContainNumber(binding.textInputEditTextPassword.text.toString())) {
-            binding.textInputLayoutPassword.error = "Required at least 1 digit"
-            binding.textInputEditTextPassword.requestFocus()
-            return false
-        } else if (!isStringLowerAndUpperCase(binding.textInputEditTextPassword.text.toString())) {
-            binding.textInputLayoutPassword.error =
-                "Password must contain upper and lower case letters"
-            binding.textInputEditTextPassword.requestFocus()
-            return false
-        } else {
+        val password = binding.textInputEditTextPassword.text.toString()
+        val isEmptyPassword = password.trim().isEmpty()
+        return if (!isEmptyPassword && password.length >= 8 && password.any(Char::isDigit)
+            && password.any(Char::isLowerCase) && password.any(Char::isUpperCase)) {
             binding.textInputLayoutPassword.isErrorEnabled = false
+            true
+        } else {
+            binding.textInputLayoutPassword.error = when {
+                isEmptyPassword -> resources.getString(R.string.required_field)
+                password.length < 8 -> resources.getString(R.string.password_size)
+                password.none(Char::isDigit) -> resources.getString(R.string.required_digit_password)
+                else -> resources.getString(R.string.password_rules)
+            }
+            if (!isEmptyPassword) binding.textInputEditTextPassword.requestFocus()
+            false
         }
-        return true
-    }
-
-    private fun isValidEmail(target: CharSequence): Boolean {
-        return Patterns.EMAIL_ADDRESS.matcher(target).matches()
-    }
-
-    private fun isStringLowerAndUpperCase(target: CharSequence): Boolean {
-        return target.any(Char::isLowerCase) && target.any(Char::isUpperCase)
-    }
-
-    private fun isStringContainNumber(target: CharSequence): Boolean {
-        return target.any(Char::isDigit)
     }
 }
